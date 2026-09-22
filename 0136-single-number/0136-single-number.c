fe41f275cab73db061c1e13c7099ec206af63bb4
Suraj Peharkar <surajpeharkar7@gmail.com>
2026-09-22 23:05:00 +0530

    Time: 1062 ms (8.15%), Space: 10 MB (92.79%) - LeetHub

---
+++ 0136-single-number/0136-single-number.c
@@ -0,0 +1,12 @@
+int singleNumber(int* nums, int numsSize) {
+    for(int i=0;i<numsSize;i++){
+        int count =0;
+            for(int j=0;j<numsSize;j++){
+                if(nums[i]==nums[j])
+                 count++;
+            }
+            if(count==1)
+            return nums[i];
+    }
+    return 0;
+};
